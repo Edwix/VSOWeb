888e55a1385bf033fdd5a2ddffd7097d7ae4237d
Edwin MOULIN <edwin.moulin@transport.alstom.com>
2015-07-16 14:05:17 +0200

    Remove unless and complicated function to make easier way

--- VSOWeb/VariableObserver.js
+++ VSOWeb/VariableObserver.js
@@ -1,33 +1,11 @@
-// A simple templating method for replacing placeholders enclosed in curly braces.
-if (!String.prototype.supplant) {
-    String.prototype.supplant = function (o) {
-        return this.replace(/{([^{}]*)}/g,
-            function (a, b) {
-                var r = o[b];
-                return typeof r === 'string' || typeof r === 'number' ? r : a || typeof r === 'boolean' ? r : a;
-            }
-        );
-    };
-}
-
 $(function () {
     //$.connection.myVariableObserverMini correspond à la ligne :
     //[HubName("myVariableObserverMini")] dans VariableObserverHub
     var ticker = $.connection.myVariableObserverMini, // the generated client-side hub proxy
     $varTable = $('#variableTable'),
     //On cherche la balise tbody du tableau avec l'id variableTable
-        $varTableBody = $varTable.find('tbody'),
-    //On définit le template pour chaque ligne
-        rowTemplate = '<tr id="{PathName}"><td>{PathName}</td><td>{ValObs}</td><td>{IsForced}</td></tr>';
+        $varTableBody = $varTable.find('tbody');
 
-    function formatDataObserver(dObs)
-    {
-        return $.extend(dObs, {
-            PathName: dObs.PathName,
-            ValObs: dObs.DValueObs,
-            IsForced: dObs.IsForced
-        });
-    }
 
     function init()
     {
@@ -35,8 +13,7 @@ $(function () {
             $varTableBody.empty();
 
             $.each(listDObs, function () {
-                var dObs = formatDataObserver(this);
-                $varTableBody.append(rowTemplate.supplant(dObs));
+                
             });
 
         });
@@ -44,11 +21,7 @@ $(function () {
 
     // Add a client-side hub method that the server will call
     ticker.client.updateDataObserver = function (dobs) {
-        /*var displayStock = formatDataObserver(dobs),
-            $row = $(rowTemplate.supplant(displayStock));
 
-        $varTableBody.find('tr[data-symbol=' + dobs.PathName + ']')
-            .replaceWith($row);*/
 
         
     }
